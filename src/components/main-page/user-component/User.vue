50cff1bcb254378ca861397d21f4f6df49b13a0d
Maxim Molokanov <maxim.molokanov@smart-com.su>
2020-04-16 01:02:15 +0300

    fix styles

--- src/components/main-page/user-component/User.vue
+++ src/components/main-page/user-component/User.vue
@@ -102,7 +102,7 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .component-block {
-  height: 300px;
+  /* height: 300px; */
   width: 500px;
   border: 1px solid green;
   padding: 10px;
